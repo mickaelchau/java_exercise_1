016d4fe952c5a794219194a8b8add5b0406660a6
Mickael Chau <mickaelchau4@gmail.com>
2022-02-10 12:31:23 +0100

    Added a while loop into Launcher class

--- src/Launcher.java
+++ src/Launcher.java
@@ -4,9 +4,12 @@ public class Launcher {
     public static void main(String[] args) {
         System.out.println("Welcome to all IN1 Students");
         Scanner stdin = new Scanner(System.in);
-        String user_input = stdin.next();
-        if (user_input.compareTo("quit") != 0)
+        while (true) {
+            String user_input = stdin.next();
+            if (user_input.compareTo("quit") == 0)
+                break;
             System.out.println("Unknown command");
+        }
         stdin.close();
     }
 }
